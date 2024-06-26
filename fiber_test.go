4484475ddd9fa3d628501d7e5331d6e80b50cd9b
syauqidamario <syauqi.djohan@student.umn.ac.id>
2024-06-26 22:22:33 +0700

    Error handling in Fiber

--- fiber_test.go
+++ fiber_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"io"
 	"mime/multipart"
 	"net/http"
@@ -14,7 +15,13 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var app = fiber.New()
+var app = fiber.New(fiber.Config{
+	// Views: engine,
+	ErrorHandler: func(ctx *fiber.Ctx, err error) error {
+		ctx.Status(fiber.StatusInternalServerError)
+		return ctx.SendString("Error : " + err.Error())
+	},
+})
 
 func TestRoutingHelloWorld(t *testing.T) {
 	app.Get("/", func(ctx *fiber.Ctx) error {
@@ -296,4 +303,19 @@ func TestStatic(t *testing.T){
 	bytes, err := io.ReadAll(response.Body)
 	assert.Nil(t, err)
 	assert.Equal(t, "This is sample text", string(bytes))
+}
+
+func TestErrorHandler(t *testing.T) {
+	app.Get("/error", func(ctx *fiber.Ctx) error {
+		return errors.New("ups")
+	})
+
+	request := httptest.NewRequest("GET", "/error", nil)
+	response, err := app.Test(request)
+	assert.Nil(t, err)
+	assert.Equal(t, 500, response.StatusCode)
+
+	bytes, err := io.ReadAll(response.Body)
+	assert.Nil(t, err)
+	assert.Equal(t, "Error : ups", string(bytes))
 }
