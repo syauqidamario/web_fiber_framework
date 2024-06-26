371d914a3b40f0a14bbcc46c6e7a477717605076
syauqidamario <syauqi.djohan@student.umn.ac.id>
2024-06-26 22:19:30 +0700

    Pre-fork in Fiber

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
@@ -11,7 +12,14 @@ func main() {
 		IdleTimeout: time.Second * 5,
 		ReadTimeout: time.Second * 5,
 		WriteTimeout: time.Second * 5,
+		Prefork: true,
 	})
+
+	if fiber.IsChild(){
+		fmt.Println("I'm a child process")
+	} else {
+		fmt.Println("I'm a parent process")
+	}
 
 	err := app.Listen("localhost:3000")
 	if err != nil {
