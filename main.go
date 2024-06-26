0aba109412d3d234e07c63e7f2db44a7f9a1bb97
syauqidamario <syauqi.djohan@student.umn.ac.id>
2024-06-26 22:29:52 +0700

    Middleware

--- main.go
+++ main.go
@@ -9,16 +9,27 @@ import (
 
 func main() {
 	app := fiber.New(fiber.Config{
-		IdleTimeout: time.Second * 5,
-		ReadTimeout: time.Second * 5,
+		IdleTimeout:  time.Second * 5,
 		WriteTimeout: time.Second * 5,
-		Prefork: true,
+		ReadTimeout:  time.Second * 5,
+		Prefork:      true,
 	})
 
-	if fiber.IsChild(){
-		fmt.Println("I'm a child process")
+	app.Use("/api", func(ctx *fiber.Ctx) error {
+		fmt.Println("I'm middleware before processing request")
+		err := ctx.Next()
+		fmt.Println("I'm middleware after processing request")
+		return err
+	})
+
+	app.Get("/api/hello", func(ctx *fiber.Ctx) error {
+		return ctx.SendString("Hello World")
+	})
+
+	if fiber.IsChild() {
+		fmt.Println("I'm child process")
 	} else {
-		fmt.Println("I'm a parent process")
+		fmt.Println("I'm parent process")
 	}
 
 	err := app.Listen("localhost:3000")
